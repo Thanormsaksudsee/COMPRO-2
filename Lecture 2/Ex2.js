da1b7f378ccfb2cddbed9dc7e54db4844bdf4dc2
Bigkuma <s6506022420037@email.kmutnb.ac.th>
2023-07-11 12:02:06 +0700

    Lecture2

--- Lecture 2/Ex2.js
+++ Lecture 2/Ex2.js
@@ -1,8 +1,8 @@
-function equation(n1, n2) {
-    
-
+function equation(n1) {
+   console.log(eval(n1))
 }
 
-console.log(equation("1+1"));
-console.log(equation("7*4-2"));
-console.log(equation("1+1+1+1+1"));
+equation("1+1");
+equation("7*4-2");
+equation("1+1+1+1+1");
+
