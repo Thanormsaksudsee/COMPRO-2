20ad4047f642125349dfdfc056835d310fda3ad2
Bigkuma <s6506022420037@email.kmutnb.ac.th>
2023-07-18 11:41:30 +0700

    ex1

--- Lecture 3/Ex1.js
+++ Lecture 3/Ex1.js
@@ -1,24 +1,53 @@
+// // let values1 = ['Apple', 1, false];
+// // let values2 = ['Fries', 2, true];
+// // let values3 = ['Mars', 9, 'Apple'];
+
+// // function compareArrays(arr1, arr2) {
+// //     let result = true;
+// //     if (arr1.length != arr2.length) {
+// //         result = false;
+// //     } else {
+// //         for (let i = 0; i < arr1.length; i++) {
+// //             if (arr1[i] !== arr2[i]) {
+// //                 result = false;
+// //             }
+// //         }
+// //     }
+// //     return result;
+// // }
+
+
+// // console.log(common(values1, values2));
+// // console.log(common(values1, values3));
+// // console.log(common(values2, values3));
+
+
+// let values1 = ['Apple', 1, false];
+// let values2 = ['Fries', 2, true];
+// let values3 = ['Mars', 9, 'Apple'];
+
+// function compre{
+//     for values1 in word
+// }
+
 let values1 = ['Apple', 1, false];
-let values2 = ['Fries', 2, true];
+let values2 = ['Fries', 9, true,'Mars'];
 let values3 = ['Mars', 9, 'Apple'];
 
-function compareArrays(arr1, arr2) {
-    let result = true;
-    if (arr1.length != arr2.length) {
-        result = false;
-    } else {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) {
-                result = false;
-            }
-        }
-    }
-    return result;
+for (let i = 0; i < values1.length; i++) { 
+  if (values2 == values1[i]) {
+    console.log(`The values1 เหมือนกับ values2, ที่คำว่า: ${values1[i]}`);
+  }
+  if (values3.includes(values1[i])) {
+    console.log(`The values1 เหมือนกับ values3, ที่คำว่า: ${values1[i]}`);
+  }
+}
+for (let i = 0; i < values2.length; i++) {
+  if (values3.includes(values2[i])) {
+    console.log(`The values2 เหมือนกับ values3,ที่คำว่า: ${values2[i]}`);
+  }
 }
 
 
-console.log(common(values1, values2));
-console.log(common(values1, values3));
-console.log(common(values2, values3));
 
 
