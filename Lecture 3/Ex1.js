68d6a4d86d275d89c315ab7dd4fecd88e3fe9dc6
BigKuma <109493795+Thanormsaksudsee@users.noreply.github.com>
2023-07-18 22:31:41 +0700

    ex1 up

--- Lecture 3/Ex1.js
+++ Lecture 3/Ex1.js
@@ -1,52 +1,49 @@
-// // let values1 = ['Apple', 1, false];
-// // let values2 = ['Fries', 2, true];
-// // let values3 = ['Mars', 9, 'Apple'];
-
-// // function compareArrays(arr1, arr2) {
-// //     let result = true;
-// //     if (arr1.length != arr2.length) {
-// //         result = false;
-// //     } else {
-// //         for (let i = 0; i < arr1.length; i++) {
-// //             if (arr1[i] !== arr2[i]) {
-// //                 result = false;
-// //             }
-// //         }
-// //     }
-// //     return result;
-// // }
-
-
-// // console.log(common(values1, values2));
-// // console.log(common(values1, values3));
-// // console.log(common(values2, values3));
-
-
-// let values1 = ['Apple', 1, false];
-// let values2 = ['Fries', 2, true];
-// let values3 = ['Mars', 9, 'Apple'];
-
-// function compre{
-//     for values1 in word
-// }
-
 let values1 = ['Apple', 1, false];
 let values2 = ['Fries', 9, true,'Mars'];
 let values3 = ['Mars', 9, 'Apple'];
 
-for (let i = 0; i < values1.length; i++) { 
-  if (values2 == values1[i]) {
-    console.log(`The values1 เหมือนกับ values2, ที่คำว่า: ${values1[i]}`);
-  }
-  if (values3.includes(values1[i])) {
-    console.log(`The values1 เหมือนกับ values3, ที่คำว่า: ${values1[i]}`);
-  }
+
+for (let i = 0;  i < values1.length; i++){
+    for (let j = 0; j < values2.length; j++) {
+        if (values1[i] === values2[j]){
+            console.log(`The values1 เหมือนกับ values2,ที่คำว่า: ${values1[i]}`);
+        }
+    }
 }
-for (let i = 0; i < values2.length; i++) {
-  if (values3.includes(values2[i])) {
-    console.log(`The values2 เหมือนกับ values3,ที่คำว่า: ${values2[i]}`);
-  }
+
+for (let i = 0;  i < values1.length; i++){
+    for (let j = 0; j < values3.length; j++) {
+        if (values1[i] === values3[j]){
+            console.log(`The values1 เหมือนกับ values3,ที่คำว่า: ${values1[i]}`);
+        }
+    }
 }
+
+for (let i = 0;  i < values2.length; i++){
+    for (let j = 0; j < values3.length; j++) {
+        if (values2[i] === values3[j]){
+            console.log(`The values2 เหมือนกับ values3,ที่คำว่า: ${values2[i]}`);
+        }
+    }
+}
+
+
+
+
+
+// for (let i = 0; i < values1.length; i++) { 
+//   if (values2 == values1[i]) {
+//     console.log(`The values1 เหมือนกับ values2, ที่คำว่า: ${values1[i]}`);
+//   }
+//   if (values3.includes(values1[i])) {
+//     console.log(`The values1 เหมือนกับ values3, ที่คำว่า: ${values1[i]}`);
+//   }
+// }
+// for (let i = 0; i < values2.length; i++) {
+//   if (values3.includes(values2[i])) {
+//     console.log(`The values2 เหมือนกับ values3,ที่คำว่า: ${values2[i]}`);
+//   }
+// }
 
 
 
