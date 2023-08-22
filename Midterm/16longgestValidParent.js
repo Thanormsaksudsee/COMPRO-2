23d5f16fc0ac8de3000538411aa1f8d208a3eb76
Bigkuma <s6506022420037@email.kmutnb.ac.th>
2023-08-22 12:01:48 +0700

    ca

--- Midterm/16longgestValidParent.js
+++ Midterm/16longgestValidParent.js
@@ -1,26 +1,41 @@
-function longestValidParentheses(s) {
-    const stack = [];
-    let maxLength = 0;
+// function longestValidParentheses(s) {
+//     const stack = [];
+//     let maxLength = 0;
     
-    stack.push(-1);
+//     stack.push(-1);
   
-    for (let i = 0; i < s.length; i++) {
-      if (s[i] === "(") {
-        stack.push(i);
-      } else {
-        stack.pop();
-        if (stack.length === 0) {
-          stack.push(i);
-        } else {
-          maxLength = Math.max(maxLength, i - stack[stack.length - 1]);
-        }
-      }
-    }
+//     for (let i = 0; i < s.length; i++) {
+//       if (s[i] === "(") {
+//         stack.push(i);
+//       } else {
+//         stack.pop();
+//         if (stack.length === 0) {
+//           stack.push(i);
+//         } else {
+//           maxLength = Math.max(maxLength, i - stack[stack.length - 1]);
+//         }
+//       }
+//     }
     
-    return maxLength;
-  }
+//     return maxLength;
+//   }
   
-  console.log(longestValidParentheses("(()")); // 2
-  console.log(longestValidParentheses(")()())")); // 4
-  console.log(longestValidParentheses("()))))(()())(")); // 6
+//   console.log(longestValidParentheses("(()")); // 2
+//   console.log(longestValidParentheses(")()())")); // 4
+//   console.log(longestValidParentheses("()))))(()())(")); // 6
   
+
+
+
+
+// ตัวอย่างการใช้ Math.max กับตัวเลข
+let maxNumber = Math.max(10, 5, 20, 8, 15);
+console.log(maxNumber); // 20
+
+// ตัวอย่างการใช้ Math.max กับตัวแปร
+let a = 30;
+let b = 15;
+let c = 25;
+let maxVariable = Math.max(a, b, c);
+console.log(maxVariable); // 30
+
